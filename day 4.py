c03d613c11d0bab721ec1ec1fdb68e0408b9c8e5
PriyankaVijayPatankar <88427925+PriyankaVijayPatankar@users.noreply.github.com>
2022-11-29 19:32:55 +0530

    Create day 4.py

---
+++ day 4.py
@@ -0,0 +1,10 @@
+# Day 4
+# Write a program to identify if the number is positive or negative
+
+inp = int(input())
+if inp>0:
+    print("Positive number")
+elif inp<0:
+    print("Negative number")
+else:
+    print("Neither positive nor negative")
