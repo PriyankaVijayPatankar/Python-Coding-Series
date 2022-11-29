20f39cafdab5a21899bf393da88ecdcec4073613
PriyankaVijayPatankar <88427925+PriyankaVijayPatankar@users.noreply.github.com>
2022-11-29 19:33:58 +0530

    Create day 5.py

---
+++ day 5.py
@@ -0,0 +1,9 @@
+# Day 5 - 20/11/2022
+# Write a program to identify if the number is even or odd
+
+inp = int(input("Enter a number: "))
+
+if inp%2==0:
+    print(inp,"is an even number")
+else:
+    print(inp,"is an odd number")
