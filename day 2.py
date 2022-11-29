45a9eac9bcab8937ec95621f26826e7418e86764
PriyankaVijayPatankar <88427925+PriyankaVijayPatankar@users.noreply.github.com>
2022-11-29 19:31:41 +0530

    Create day 2.py

---
+++ day 2.py
@@ -0,0 +1,8 @@
+# Day 2
+# Write a program to identify if the character is an alphabet or not.
+ 
+inp=input("Enter a value: ")
+if ((inp>='a' and inp<='z') or (inp>='A' and inp<='Z') ):
+    print("Alphabet")
+else:
+    print("Not an alphabet")
