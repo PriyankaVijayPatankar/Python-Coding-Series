f1a9fd878733d5b9077ecf9e92acc8631a350356
Priyanka Patankar <88427925+PriyankaVijayPatankar@users.noreply.github.com>
2022-12-12 22:33:17 +0530

    Day 27 code uploaded

---
+++ day 27.py
@@ -0,0 +1,5 @@
+# Day 27 
+# Write a program to find the double of the given number without using arithmetic operator
+
+num = int(input("Enter a number: "))
+print("Double of input number is: ",num<<1)
