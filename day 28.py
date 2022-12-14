e9ed3c43d9f9bcf7388424482e10397e99f5c912
Priyanka Patankar <88427925+PriyankaVijayPatankar@users.noreply.github.com>
2022-12-14 19:24:37 +0530

    Day 28, 29 codes uploaded

---
+++ day 28.py
@@ -0,0 +1,7 @@
+# Day 28
+# Write a program to reverse a string
+
+str1 = input("Enter a string: ")
+for i in str1:
+    str2=i+str2
+print(str2)
---
+++ day 29.py
@@ -0,0 +1,6 @@
+# Day 29
+# Write a program to concatenate a string
+
+str1 = input("Enter a string: ")
+str2 = input("Enter another string: ")
+print("After concatenation,",str1+str2)
