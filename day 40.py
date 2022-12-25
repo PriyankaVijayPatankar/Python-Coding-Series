6e0ca1aceb11492be90a606af52e8326de2369bd
Priyanka Patankar <88427925+PriyankaVijayPatankar@users.noreply.github.com>
2022-12-25 23:02:54 +0530

    Day 40 code uploaded

---
+++ day 40.py
@@ -0,0 +1,9 @@
+# Day 40
+# Write a program to replace substring in a string
+
+s1 = input("Enter a string: ")
+s2 = input("Enter the substring to be removed: ")
+s3 = input("Enter a new substring: ")
+
+s4 = s1.replace(s2,s3)
+print(s4)
